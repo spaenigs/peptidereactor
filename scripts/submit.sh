5ca5d5104a53370e9f467356b6bf8575cf0e4e57
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2019-07-31 15:47:55 +0200

    adjusted submission parameters

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 #$ -S /bin/bash
-#$ -l h_vmem=750M
-#$ -pe smp 16
-#$ -l h_rt=86400
+#$ -l h_vmem=500M
+#$ -pe smp 32
+#$ -l h_rt=259200
 #$ -e /scratch/spaenigs/error
 #$ -o /scratch/spaenigs/out
+#$ -wd /scratch/spaenigs/eb/
 
 . /etc/profile.d/modules.sh
 module load tools/R-3.4.3
@@ -13,5 +14,5 @@ module load tools/R-3.4.3
 source .bashrc
 conda activate encoding_benchmark
 
-snakemake --config dataset=neuropeptides part=ds1 normalize=yes --cores 16 -np
+snakemake --config dataset=neuropeptides part=ds1 normalize=yes --cores 32 -np
 
