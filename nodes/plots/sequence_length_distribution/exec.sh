dd09895836921b300afc25eb77d6d63643d0ed4a
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2019-08-27 17:02:40 +0200

    removed version outputs

--- nodes/plots/sequence_length_distribution/exec.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -v /home/spaenigs/PycharmProjects/eb/nodes/plots/sequence_length_distribution/snakemake:/snakemake \
-           -u `id -u $USER` \
-           $IMAGE_NAME \
-           $1
--- nodes/utils/split_normalize/exec.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -v /home/spaenigs/PycharmProjects/eb/nodes/utils/split_normalize/snakemake:/snakemake \
-           -u `id -u $USER` \
-           node_utils_split_normalize_image \
-           $1
