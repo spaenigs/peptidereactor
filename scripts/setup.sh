423a856baf839acf265a24afa048fb2ef4ea293f
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2019-09-09 14:40:19 +0200

    moved Dockerfile to nodes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,33 +11,33 @@ image_name=node_$conda_env\_image
 
 export IMAGE_NAME=$image_name
 
-touch environment.yaml
-cat > environment.yaml <<- EOF
-name: $conda_env
-$3
-EOF
+#touch environment.yaml
+#cat > environment.yaml <<- EOF
+#name: $conda_env
+#$3
+#EOF
 
-touch Dockerfile
-cat > Dockerfile <<- EOF
-FROM continuumio/miniconda3
-ADD environment.yaml environment.yaml
-RUN conda update -y conda \
-    && conda env create -f environment.yaml \
-    && echo "conda activate $conda_env" >> ~/.bashrc
-ENV PATH /opt/conda/envs/$conda_env/bin:\$PATH
-RUN echo \$PATH
-RUN conda env list
-VOLUME /snakemake
-WORKDIR /snakemake
-ENTRYPOINT ["snakemake"]
-EOF
+#touch Dockerfile
+#cat > Dockerfile <<- EOF
+#FROM continuumio/miniconda3
+#ADD environment.yaml environment.yaml
+#RUN conda update -y conda \
+#    && conda env create -f environment.yaml \
+#    && echo "conda activate $conda_env" >> ~/.bashrc
+#ENV PATH /opt/conda/envs/$conda_env/bin:\$PATH
+#RUN echo \$PATH
+#RUN conda env list
+#VOLUME /snakemake
+#WORKDIR /snakemake
+#ENTRYPOINT ["snakemake"]
+#EOF
 
 if docker image inspect $image_name > /dev/null 2>&1; then
-  echo "Image $image_name already existing. Exit..."
+  echo "Image $image_name already existing. Skipping..."
 else
   echo "Building image $image_name..."
   docker build -t $image_name .
 fi
 
-rm Dockerfile environment.yaml
+#rm Dockerfile environment.yaml
 
