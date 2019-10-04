6f859fa5b045d6b1519c43943b2b6bc17eedb4a7
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2019-10-04 16:44:44 +0200

    remove configfile

--- utils/subworkflow.py
+++ utils/subworkflow.py
@@ -1,14 +1,15 @@
 from functools import partial
 
 import yaml
+import os
 from snakemake import Workflow
 from snakemake.workflow import Subworkflow
 
 from snakemake import snakemake as smk_func
 
 
-def set_yaml(_configfile, _input, _output):
-    with open(_configfile, mode="w") as stream:
+def set_input_output(_configfile, _input, _output):
+    with open(_configfile, mode="a") as stream:
         yaml.safe_dump({**_input, **_output}, stream)
 
 
@@ -18,7 +19,7 @@ SNAKEFILE = snakemake.params.snakefile
 CONFIGFILE = snakemake.params.configfile
 INPUT, OUTPUT = dict(snakemake.input), dict(snakemake.output)
 
-set_yaml(CONFIGFILE, INPUT, OUTPUT)
+set_input_output(CONFIGFILE, INPUT, OUTPUT)
 
 workflow = Workflow(GLOBAL_WORKDIR)
 sw = Subworkflow(workflow, name=NAME, snakefile=SNAKEFILE, workdir=GLOBAL_WORKDIR, configfile=CONFIGFILE)
@@ -34,8 +35,10 @@ def __rule_all(input, output, params, wildcards, threads, resources, log, versio
                singularity_args, use_singularity, bench_record, jobid, is_shell, bench_iteration, shadow_dir):
     pass
 
-# os.chdir(snakemake.config["global_workdir"])
-# workflow.w
+
 workflow.check()
-workflow.execute(dryrun=False, updated_files=[], quiet=True, resources=dict(),
-                 subsnakemake=partial(smk_func))
+success = workflow.execute(dryrun=False, updated_files=[], quiet=True, resources=dict(),
+                           subsnakemake=partial(smk_func))
+
+os.remove(CONFIGFILE)
+
