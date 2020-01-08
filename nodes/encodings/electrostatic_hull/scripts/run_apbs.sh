6c2b3c11d7ceae7ce6e5a43eca0f4e69220a6b2f
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-01-08 14:48:54 +0100

    added empty lines

--- nodes/encodings/electrostatic_hull/scripts/run_apbs.sh
+++ nodes/encodings/electrostatic_hull/scripts/run_apbs.sh
@@ -8,10 +8,14 @@ export TYPE=$4
 export OLD_WD=$PWD
 export PATH_TO_NODE=nodes/encodings/electrostatic_hull/;
 export NEW_WD=data/temp/$TOKEN/$RANDOM/; mkdir $NEW_WD;
+
 cp $INPUT $NEW_WD/template.pqr;
 cp $PATH_TO_NODE/config/apbs_$TYPE.input $NEW_WD;
 cd $NEW_WD;
+
 apbs apbs_$TYPE.input 1> /dev/null 2> /dev/null;
+
 cp $TYPE.dx $OLD_WD/$OUTPUT;
 cd - 1> /dev/null;
+
 rm -r $NEW_WD;
