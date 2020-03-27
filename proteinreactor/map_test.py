2bed69252d2d1dcfee4751e5c20a2c16459300cf
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-03-27 11:00:39 +0100

    added scripts temp

---
+++ proteinreactor/map_test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
+import plotly.express as px
+
+fig = px.scatter_mapbox(us_cities.iloc[:10], lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
