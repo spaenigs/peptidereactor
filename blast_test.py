48a3e44cd22d51214ffbf76cd90c24d4c405fd83
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-04-20 15:06:23 +0200

    use box plots

--- blast_test.py
+++ blast_test.py
@@ -8,31 +8,32 @@ import pandas as pd
 
 df_res = pd.DataFrame()
 
-for seq_len in range(50, 10000, 50)[:2]:
 
-    print(seq_len)
+def download_proteins(fasta="random_proteins.fasta"):
+    for seq_len in range(50, 10000, 50):
+        print(seq_len)
+        limit = 1000
+        url = f"https://www.uniprot.org/uniprot/?query=length%3A[{seq_len}+TO+{seq_len}]&format=fasta&limit={limit}"
+        response = requests.get(url)
+        fasta_handle = response.content.decode()
+        with open(fasta, "a") as f:
+            SeqIO.write(SeqIO.parse(StringIO(fasta_handle), "fasta"), f, "fasta")
 
-    limit = 5000
-    url = f"https://www.uniprot.org/uniprot/?query=length%3A[{seq_len}+TO+{seq_len}]&sort=score&format=fasta&limit={limit}"
-    response = requests.get(url)
-    fasta_handle = response.content.decode()
 
-    with open("tmp.fasta", "w") as f:
-        SeqIO.write(SeqIO.parse(StringIO(fasta_handle), "fasta"), f, "fasta")
-
-    header = ["qseqid", "sacc", "sstart", "send", "evalue", "qseq", "sseq"]
+def run_blast(fasta="random_proteins.fasta"):
+    header = ["qseqid", "sacc", "sstart", "send", "evalue", "qseq", "sseq", "qlen"]
     cline = NcbiblastpCommandline(
         db="peptidereactor/db/pdb/in_structure/pdb.db",
-        query="tmp.fasta",
-        num_threads=12,
+        query=fasta,
+        num_threads=28,
         max_target_seqs=1,
         max_hsps=1,
         outfmt="10 " + " ".join(header))
     stdout, stderr = cline()
+    df_res = pd.read_csv(StringIO(stdout), names=header, index_col=0)
+    df_res["length_2"] = range(0, df_res.shape[0])
+    df_res.to_csv(f"blast_test.csv")
 
-    df_blast = pd.read_csv(StringIO(stdout), names=header, index_col=0)
 
-    df_res = pd.concat([df_res, df_blast])
-    df_res.to_csv(f"blast_test_2.csv")
-
-df_res.to_csv(f"blast_test_2.csv")
+download_proteins()
+# run_blast()
--- blast_test_plot.py
+++ blast_test_plot.py
@@ -5,44 +5,65 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
-df_blast = pd.read_csv("blast_test.csv", index_col=0)
-df_blast["lengths"] = [int(r.split("_")[1]) for r in df_blast.index]
-df_blast["lengths_2"] = range(0, len(df_blast.index))
-df_blast["evalues_2"] = [0 if i == 0 else np.log(i) * (-1) for i in df_blast.evalues]
+df_blast = pd.read_csv("blast_test.csv", sep=",", index_col=0)
+# df_blast["lengths"] = [int(r.split("_")[1]) for r in df_blast.index]
+# df_blast["lengths_2"] = range(0, len(df_blast.index))
 
-# Create figure with secondary y-axis
-fig = make_subplots(specs=[[{"secondary_y": True}]])
+# TODO find better way, since np.abs(np.log(0.000000000000000000000000000000000000000000000009)) == 108.33
+df_blast["evalue_2"] = [0 if i == 0 else np.abs(np.log(i)) for i in df_blast.evalue]
 
-tl_type = "lowess" # "ols"
-tmp = px.scatter(df_blast, x="lengths_2", y="evalues_2", trendline=tl_type, trendline_color_override="rgba(208,28,139,1)")
-scatter = tmp.data[0]
-scatter.marker.color = "rgba(208,28,139,0.1)"
-trendline = tmp.data[1]
+print(df_blast.loc[df_blast.qlen == 8750, :].sort_values(by="evalue").iloc[0, -5:-3])
 
-fig.add_trace(
-    scatter,
-    secondary_y=False,
-)
+fig = go.Figure()
 
-fig.add_trace(trendline)
+df_blast = df_blast.loc[df_blast.qlen > 5000, :]
 
-indices, no_hits = [], []
-for i in set([r.split("_")[1] for r in df_blast.index]):
-    row_idx = int(np.median(df_blast.filter(like=f"len_{i}", axis=0)["lengths_2"].values))
-    indices += [row_idx]
-    no_hits += [df_blast.iloc[row_idx, 1]]
+df_blast.qlen = [str(i) for i in df_blast.qlen]
 
-sorted_tuples = sorted(zip(indices, no_hits), key=lambda tup: tup[0])
-indices_sorted, no_hits_sorted = zip(*sorted_tuples)
 
-fig.add_trace(
-    go.Scatter(x=indices_sorted, y=no_hits_sorted, name="# of no hits", marker_color="rgba(77,172,38,1)"),
-    secondary_y=True,
-)
 
+
+for i in set(df_blast.qlen):
+    fig.add_trace(
+        go.Box(y=df_blast.loc[df_blast.qlen == i, "evalue"], name=str(i))
+    )
+
+
+
+# # Create figure with secondary y-axis
+# fig = make_subplots(specs=[[{"secondary_y": True}]])
+#
+# tl_type = "ols" # "ols"/"lowess"
+# tmp = px.scatter(df_blast, x="length_2", y="evalues_2", trendline=tl_type, trendline_color_override="rgba(208,28,139,1)")
+# scatter = tmp.data[0]
+# scatter.marker.color = "rgba(208,28,139,0.1)"
+# trendline = tmp.data[1]
+#
+# fig.add_trace(
+#     scatter,
+#     secondary_y=False,
+# )
+
+# fig.add_trace(trendline)
+
+# indices, no_hits = [], []
+# for i in df_blast.length:
+#     row_idx = int(np.median(df_blast.filter(like=str(i), axis=0)["length_2"].values))
+#     indices += [row_idx]
+#     no_hits += [df_blast.iloc[row_idx, 1]]
+#
+# sorted_tuples = sorted(zip(indices, no_hits), key=lambda tup: tup[0])
+# indices_sorted, no_hits_sorted = zip(*sorted_tuples)
+#
+# fig.add_trace(
+#     go.Scatter(x=indices_sorted, y=no_hits_sorted, name="# of no hits", marker_color="rgba(77,172,38,1)"),
+#     secondary_y=True,
+# )
+#
 fig.update_layout(
     xaxis_title="Sequence length",
-    yaxis_title="log(e-value) * (-1): the smaller, the better",
+    # yaxis_title="log(e-value) * (-1): the smaller, the better",
+    yaxis_type="log",
     font=dict(
         size=18,
     )
