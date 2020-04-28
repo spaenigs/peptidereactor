1d73da047b3bab3410f247da7847c7a9fed515f0
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-04-28 16:50:19 +0200

    added rule template

---
+++ nodes/encodings/asa/__init__.py
@@ -0,0 +1,24 @@
+import textwrap
+
+
+def rule(fasta_in, classes_in, profile_dir, asa_out):
+
+    rule = textwrap.dedent(
+        f'''\
+            rule encoding_asa:
+                input:
+                     fasta_in="{fasta_in}",
+                     classes_in="{classes_in}",
+                     profile="{profile_dir}"
+                output:
+                     csv_out="{asa_out}
+                params:
+                     snakefile="nodes/encodings/asa/asa.smk",
+                     configfile="nodes/encodings/asa/config.yaml"
+                run:
+                     with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+                         shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+
+            ''')
+
+    return rule
