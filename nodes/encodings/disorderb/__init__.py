7d7501be792571ea6de7f049d061fab0a491dfba
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-04-29 08:51:20 +0200

    added rule template

---
+++ nodes/encodings/disorderb/__init__.py
@@ -0,0 +1,24 @@
+import textwrap
+
+
+def rule(fasta_in, classes_in, profile_dir, disorderb_out):
+
+    rule = textwrap.dedent(
+        f'''\
+            rule encoding_disorderb:
+                input:
+                     fasta_in="{fasta_in}",
+                     classes_in="{classes_in}",
+                     profile="{profile_dir}"
+                output:
+                     csv_out="{disorderb_out}"
+                params:
+                     snakefile="nodes/encodings/disorderb/disorderb.smk",
+                     configfile="nodes/encodings/disorderb/config.yaml"
+                run:
+                     with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+                         shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+
+            ''')
+
+    return rule
