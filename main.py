4721ff09534248fc4cd4b4a14b46376e49874267
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-04-30 19:13:41 +0200

    compute msa after structure prediction

--- main.py
+++ main.py
@@ -21,31 +21,38 @@ with WorkflowSetter(cores=CORES) as w:
         fasta_out="data/{dataset}/seqs_mapped.fasta",
         maps_out="data/{dataset}/misc/mapped_sequence_names.yaml"))
 
-    w.add(utils.multiple_sequence_alignment.rule(
-        fasta_in="data/{dataset}/seqs_mapped.fasta",
-        classes_in="data/{dataset}/classes.txt",
-        fasta_out="data/{dataset}/seqs_msa.fasta"))
+    w.add(utils.tertiary_structure_search.rule(
+        fasta_in="data/{dataset}/seqs_mapped.fasta", classes_in="data/{dataset}/classes.txt",
+        fasta_sec_out="data/{dataset}/seqs_sec.fasta", classes_sec_out="data/{dataset}/classes_sec.txt",
+        fasta_ter_out="data/{dataset}/seqs_ter.fasta", classes_ter_out="data/{dataset}/classes_ter.txt",
+        pdb_dir="data/{dataset}/pdb/", profile_dir="data/{dataset}/profile/"))
 
-    seqb = encodings.sequence_based.rule.Rule()
+    w.add(utils.multiple_sequence_alignment.rule(
+        fastas_in=["data/{dataset}/seqs_mapped.fasta", "data/{dataset}/seqs_sec.fasta",
+                   "data/{dataset}/seqs_ter.fasta"],
+        fastas_out=["data/{dataset}/seqs_msa.fasta", "data/{dataset}/seqs_msa_sec.fasta",
+                    "data/{dataset}/seqs_msa_ter.fasta"]))
+
+    seqb = encodings.sequence_based.Rule()
     w.add(seqb.rule(
         fasta_in="data/{dataset}/seqs_mapped.fasta", fasta_msa_in="data/{dataset}/seqs_msa.fasta",
         classes_in="data/{dataset}/classes.txt", path_to_config="config.yaml",
         misc_dir="data/{dataset}/misc/", csv_dir="data/{dataset}/csv/"))
 
-    w.add(utils.tertiary_structure_search.rule(
-        fasta_in="data/{dataset}/seqs_mapped.fasta", classes_in="data/{dataset}/classes.txt",
-        fasta_out="data/{dataset}/annotated_seqs.fasta", classes_out="data/{dataset}/annotated_classes.txt",
-        pdb_dir="data/{dataset}/pdb/", profile_dir="data/{dataset}/profile/"))
-
     strb = encodings.structure_based.Rule()
     w.add(strb.rule(
-        fasta_in="data/{dataset}/seqs_mapped.fasta", fasta_msa_in="data/{dataset}/seqs_msa.fasta",
-        classes_in="data/{dataset}/classes.txt", path_to_config="config.yaml", pdb_dir="data/{dataset}/pdb/",
+        include=["asa", "distance_distribution"],
+        fasta_sec_in="data/{dataset}/seqs_sec.fasta", fasta_msa_sec_in="data/{dataset}/seqs_msa_sec.fasta",
+        classes_sec_in="data/{dataset}/classes_sec.txt", fasta_ter_in="data/{dataset}/seqs_ter.fasta",
+        classes_ter_in="data/{dataset}/classes_ter.txt", path_to_config="config.yaml", pdb_dir="data/{dataset}/pdb/",
         profile_dir="data/{dataset}/profile/", csv_dir="data/{dataset}/csv/"))
 
 target = \
-    expand(seqb.target_csvs, dataset=DATASETS) + \
-    expand(strb.target_csvs, dataset=DATASETS)
+    expand(strb.target_csvs, dataset=DATASETS) + \
+    expand(seqb.target_csvs, dataset=DATASETS)
+
+target += expand(["data/{dataset}/seqs_msa.fasta", "data/{dataset}/seqs_msa_sec.fasta", "data/{dataset}/seqs_msa_ter.fasta"], dataset=DATASETS)
+target += expand(["data/{dataset}/seqs_sec.fasta", "data/{dataset}/classes_sec.txt", "data/{dataset}/seqs_ter.fasta", "data/{dataset}/classes_ter.txt"], dataset=DATASETS)
 
 with WorkflowExecuter(dict(), dict(out=target), "peptidereactor.yaml", cores=CORES) as e:
     shell(f"""./peptidereactor/run_pipeline -s peptidereactor.smk --configfile peptidereactor.yaml {" ".join(sys.argv[1:])}""")
