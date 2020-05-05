04bf1883d98e4ea83ff36b51c73177ba5b236bf9
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-05-05 16:23:35 +0200

    adjusted for pdb slices

--- nodes/encodings/qsar/scripts/compute_qsar.py
+++ nodes/encodings/qsar/scripts/compute_qsar.py
@@ -1,3 +1,6 @@
+import re
+import subprocess
+
 from rdkit import Chem
 from mordred import Calculator, descriptors
 from Bio.PDB import PDBParser, Dice
@@ -9,12 +12,46 @@ from snakemake.shell import shell
 
 def get_pdb_chunks(full_pdb, window_size, len_residues, token):
     len_residues += 1
-    for start, end in [[0, len_residues]] \
-            if len_residues <= window_size \
-            else enumerate(range(window_size, len_residues)):
-        filename = f"data/temp/{token}/{full_pdb.get_id()}_{start}_{end}.pdb"
-        Dice.extract(full_pdb, "A", start, end, filename)
-        yield filename, Chem.MolFromPDBFile(filename)
+
+    for chain in full_pdb.get_chains():
+        chain_id = chain.get_id()
+        start_idx, stop_idx = \
+            list(chain.get_residues())[0].get_id()[1], list(chain.get_residues())[-1].get_id()[1]
+        if len_residues <= window_size:
+            windows = [[start_idx, stop_idx]]
+        else:
+            tmp = enumerate(range(window_size, len_residues))
+            windows = [(i+start_idx, j+start_idx) for i, j in tmp]
+        for start, end in windows:
+            filename = f"data/temp/{token}/{full_pdb.get_id()}_{start}_{end}.pdb"
+            Dice.extract(full_pdb, chain_id, start, end, filename)
+
+            cmd = f"from rdkit import Chem; Chem.MolFromPDBFile('{filename}')"
+            process = subprocess.Popen(["python", "-c", cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+
+            error_msg = stderr.decode()
+
+            if error_msg == "":
+                mol_obj = Chem.MolFromPDBFile(filename)
+
+            else:
+                hits = re.findall("Explicit valence for atom # (\d+)", error_msg)
+                if len(hits) > 0:
+                    line_number = int(hits[0])
+                    new_filename = filename.replace(".pdb", ".tmp.pdb")
+                    with open(filename) as f1, open(new_filename, "a") as f2:
+                        for i, l in enumerate(f1.readlines()):
+                            if i == line_number:
+                                continue
+                            else:
+                                f2.write(l)
+                                f2.flush()
+                    mol_obj = Chem.MolFromPDBFile(new_filename)
+                else:
+                    raise ValueError(f"Error parsing rdkit error message: {error_msg}!")
+
+            yield filename, mol_obj
 
 
 def clean_up(filenames):
@@ -34,13 +71,18 @@ calc = Calculator(descriptors)
 len_residues = len(list(structure.get_residues()))
 filenames, pdbs = zip(*get_pdb_chunks(structure, 20, len_residues, token))
 
-df = calc.pandas(list(pdbs), quiet=True)
-df = df.loc[:, [s.dtype in [np.float, np.int] for n, s in df.items()]]
-df.dropna(axis="columns", inplace=True)
-df = pd.DataFrame(df.apply(np.mean)).transpose()
-df.index = [name]
-df["y"] = [class_]
+try:
+    df = calc.pandas(list(pdbs), quiet=True)
+    df = df.loc[:, [s.dtype in [np.float, np.int] for n, s in df.items()]]
+    df.dropna(axis="columns", inplace=True)
+    df = pd.DataFrame(df.apply(np.mean)).transpose()
+    df.index = [name]
+    df["y"] = [class_]
 
-df.to_csv(str(smk_obj.output))
+    df.to_csv(str(smk_obj.output))
 
-clean_up(filenames)
+    clean_up(filenames)
+except Exception as e:
+    print(e)
+    import pydevd_pycharm
+    pydevd_pycharm.settrace('localhost', port=8889, stdoutToServer=True, stderrToServer=True)
