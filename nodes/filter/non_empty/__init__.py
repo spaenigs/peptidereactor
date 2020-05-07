7a5cad5cf6b333931455f7d76fe7d97cf61b3e8b
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-05-07 18:07:30 +0200

    added benchmark and random rule identifier

--- nodes/filter/non_empty/__init__.py
+++ nodes/filter/non_empty/__init__.py
@@ -1,22 +1,45 @@
-import textwrap
+import secrets
 
 
-def rule(csv_in, csv_out):
+def _get_header(token):
+    return f'''
+rule filter_non_empty_{token}:
+'''
 
-    rule = textwrap.dedent(
-        f'''\
-            rule filter_non_empty:
-                input:
-                     csv_in={csv_in}
-                output:
-                     csv_out=directory("{csv_out}")
-                params:
-                     snakefile="nodes/filter/non_empty/Snakefile",
-                     configfile="nodes/filter/non_empty/config.yaml"
-                run:
-                     with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
-                         shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
 
-    ''')
+def _get_benchmark(benchmark_out):
+    return f'''\
+    benchmark:
+        "{benchmark_out}"
+'''
+
+
+def _get_main(csv_in, csv_out):
+    return f'''\
+    input:
+        csv_in={csv_in}
+    output:
+        csv_out=directory("{csv_out}")              
+    params:
+        snakefile="nodes/filter/non_empty/Snakefile",
+        configfile="nodes/filter/non_empty/config.yaml"
+    run:
+        with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+            shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+
+'''
+
+
+def rule(csv_in, csv_out, benchmark_dir=None):
+
+    token = secrets.token_hex(4)
+
+    rule = _get_header(token)
+
+    if benchmark_dir is not None:
+        benchmark_out = f"{benchmark_dir}filter_non_empty_{token}.txt"
+        rule += _get_benchmark(benchmark_out)
+
+    rule += _get_main(csv_in, csv_out)
 
     return rule
