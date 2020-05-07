61fe7f64d860290b95a7b5bed3cc201640e09707
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-05-07 13:36:15 +0200

    fixed index error

--- nodes/filter/psekraac/scripts/clustering.R
+++ nodes/filter/psekraac/scripts/clustering.R
@@ -5,5 +5,5 @@ d <- read.csv(snakemake@input[[1]], row.names = "e1")
 # get representative file by computing mediod of one cluster
 km <- pam(d, 1)
 
-write.csv(data.frame(row.names(km$medoids)), file = snakemake@output[[2]])
+write.csv(data.frame(row.names(km$medoids)), file = snakemake@output[[1]])
     
