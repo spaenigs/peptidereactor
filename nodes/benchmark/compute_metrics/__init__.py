cd09a4f752249b13a7c9bbdc37c19acd9c0f60d2
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-05-29 16:38:51 +0200

    initial commit: node benchmark: compute_metrics

---
+++ nodes/benchmark/compute_metrics/__init__.py
@@ -0,0 +1,41 @@
+import secrets
+
+
+def _get_header(token):
+    return f'''
+rule benchmark_compute_metrics_{token}:
+'''
+
+
+def _get_benchmark(benchmark_out):
+    return f'''\
+    benchmark:
+        "{benchmark_out}"
+'''
+
+
+def _get_main(csv_dir_in, metrics_dir_out):
+    return f'''\
+    input:
+        csv_dir_in="{csv_dir_in}"
+    output:
+        metrics_dir_out=directory("{metrics_dir_out}")
+    threads:
+         1000
+    params:
+        snakefile="nodes/benchmark/compute_metrics/Snakefile",
+        configfile="nodes/benchmark/compute_metrics/config.yaml"
+    run:
+        with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+            shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+'''
+
+
+def rule(csv_dir_in, metrics_dir_out, benchmark_dir=None):
+    token = secrets.token_hex(4)
+    rule = _get_header(token)
+    if benchmark_dir is not None:
+        benchmark_out = f"{benchmark_dir}benchmark_compute_metrics_{token}.txt"
+        rule += _get_benchmark(benchmark_out)
+    rule += _get_main(csv_dir_in, metrics_dir_out)
+    return rule
