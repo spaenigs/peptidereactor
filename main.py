eed87c00f30856f8ee207507002a695863b2770b
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-06-02 15:36:05 +0200

    added flag

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ DATASETS = ["hiv_protease", "ace_vaxinpad"]
 with WorkflowSetter(cores=CORES, benchmark_dir="data/{dataset}/misc/benchmark/") as w:
 
     w.add(utils.check_dataset.rule(
-        fasta_in="data/{dataset}/seqs.fasta", report_out="data/{dataset}/misc/seqs_report.txt"))
+        fasta_in="data/{dataset}/seqs.fasta", classes_in="data/{dataset}/classes.txt",
+        report_out="data/{dataset}/misc/seqs_report.txt", benchmark_dir=w.benchmark_dir))
 
     w.add(utils.map_sequence_names.rule(
         fasta_in="data/{dataset}/seqs.fasta", fasta_out="data/{dataset}/seqs_mapped.fasta",
@@ -98,4 +99,6 @@ with WorkflowSetter(cores=CORES, benchmark_dir="data/{dataset}/misc/benchmark/")
         expand(w.benchmark_dir + "benchmark.csv", dataset=DATASETS)
 
 with WorkflowExecuter(dict(), dict(out=target), "peptidereactor.yaml", cores=CORES) as e:
-    shell(f"""./peptidereactor/run_pipeline -s peptidereactor.smk --configfile peptidereactor.yaml {" ".join(sys.argv[1:])}""")
+    main_cmd = "./peptidereactor/run_pipeline -s peptidereactor.smk --configfile peptidereactor.yaml"
+    options = f""" --cores {CORES} --keep-going {" ".join(sys.argv[1:])}"""
+    shell(main_cmd + options)
