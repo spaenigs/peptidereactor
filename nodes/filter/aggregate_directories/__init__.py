f5dbf195a9fb09c5a9cd459d1bb0de1ed8e5e610
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-06-03 18:02:32 +0200

    added node: filter aggregate directories

---
+++ nodes/filter/aggregate_directories/__init__.py
@@ -0,0 +1,41 @@
+import secrets
+
+
+def _get_header(token):
+    return f'''
+rule filter_aggregate_directory_{token}:
+'''
+
+
+def _get_benchmark(benchmark_out):
+    return f'''\
+    benchmark:
+        "{benchmark_out}"
+'''
+
+
+def _get_main(dirs_in, dir_out):
+    return f'''\
+    input:
+         dirs_in={dirs_in}
+    output:
+         csv_out=directory("{dir_out}")
+    threads:
+         1000              
+    params:
+         snakefile="nodes/filter/aggregate_directory/Snakefile",
+         configfile="nodes/filter/aggregate_directory/config.yaml"
+    run:
+         with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+             shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+'''
+
+
+def rule(dirs_in, dir_out, benchmark_dir=None):
+    token = secrets.token_hex(4)
+    rule = _get_header(token)
+    if benchmark_dir is not None:
+        benchmark_out = f"{benchmark_dir}filter_aggregate_directory_{token}.txt"
+        rule += _get_benchmark(benchmark_out)
+    rule += _get_main(dirs_in, dir_out)
+    return rule
