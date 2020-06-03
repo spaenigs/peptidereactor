3a2d26e13f7357f4823ddbdfa331c50a26326485
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-06-03 18:02:06 +0200

    added node: benchmark diversity

---
+++ nodes/benchmark/diversity/__init__.py
@@ -0,0 +1,42 @@
+import secrets
+
+
+def _get_header(token):
+    return f'''
+rule benchmark_diversity_{token}:
+'''
+
+
+def _get_benchmark(benchmark_out):
+    return f'''\
+    benchmark:
+        "{benchmark_out}"
+'''
+
+
+def _get_main(group_1_in, group_2_in, csv_dir_out):
+    return f'''\
+    input:
+        group_1_in="{group_1_in}",
+        group_2_in="{group_2_in}"
+    output:
+        csv_dir_out=directory("{csv_dir_out}")
+    threads:
+         1000      
+    params:
+        snakefile="nodes/benchmark/diversity/Snakefile",
+        configfile="nodes/benchmark/diversity/config.yaml"
+    run:
+        with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+            shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+'''
+
+
+def rule(group_1_in, group_2_in, csv_dir_out, benchmark_dir=None):
+    token = secrets.token_hex(4)
+    rule = _get_header(token)
+    if benchmark_dir is not None:
+        benchmark_out = f"{benchmark_dir}benchmark_cross_validation_ensemble_{token}.txt"
+        rule += _get_benchmark(benchmark_out)
+    rule += _get_main(group_1_in, group_2_in, csv_dir_out)
+    return rule
