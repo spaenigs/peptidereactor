ac967aa053916e0b25d41e90dd054f0b986afa2e
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-06-05 14:23:20 +0200

    initial commit: test for criticial difference

---
+++ nodes/benchmark/critical_difference/scripts/friedman.R
@@ -0,0 +1,33 @@
+library(scmamp)
+library(yaml)
+
+Sys.setlocale("LC_NUMERIC","en_US.UTF-8")
+
+d <- read.csv(snakemake@input[[1]], row.names = 1)
+
+idt <- imanDavenportTest(d)
+write_yaml(
+  list(
+    method = idt$method, , statistic = idt$statistic, p.value = idt$p.value
+  ), snakemake@output[["fo"]]
+)
+
+nm <- nemenyiTest(d)
+write_yaml(
+  list(
+    method = nm$method, cd = nm$statistic
+  ), snakemake@output[["no"]]
+)
+
+dm <- nm$diff.matrix
+rownames(dm) <- colnames(dm)
+write.csv(dm, snakemake@output[["co"]])
+
+#svg(filename = "cd.svg")
+# algorithms that cannot be regarded as different are joined by a horizontal bar
+#plotCD(results.matrix = d, alpha = 0.05)
+#dev.off()
+
+#test.res <- postHocTest(data = d, test = 'friedman', correct = "holland")
+#test.res
+
