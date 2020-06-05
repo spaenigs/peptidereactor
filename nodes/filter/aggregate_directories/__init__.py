bfcb2738f0a2093580fbc7dd169e1f39dfd636a6
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-06-05 14:25:24 +0200

    added node: filter aggregate_directories

--- nodes/filter/aggregate_directories/__init__.py
+++ nodes/filter/aggregate_directories/__init__.py
@@ -3,7 +3,7 @@ import secrets
 
 def _get_header(token):
     return f'''
-rule filter_aggregate_directory_{token}:
+rule filter_aggregate_directories_{token}:
 '''
 
 
@@ -19,12 +19,12 @@ def _get_main(dirs_in, dir_out):
     input:
          dirs_in={dirs_in}
     output:
-         csv_out=directory("{dir_out}")
+         dir_out=directory("{dir_out}")
     threads:
          1000              
     params:
-         snakefile="nodes/filter/aggregate_directory/Snakefile",
-         configfile="nodes/filter/aggregate_directory/config.yaml"
+         snakefile="nodes/filter/aggregate_directories/Snakefile",
+         configfile="nodes/filter/aggregate_directories/config.yaml"
     run:
          with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
              shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
@@ -35,7 +35,7 @@ def rule(dirs_in, dir_out, benchmark_dir=None):
     token = secrets.token_hex(4)
     rule = _get_header(token)
     if benchmark_dir is not None:
-        benchmark_out = f"{benchmark_dir}filter_aggregate_directory_{token}.txt"
+        benchmark_out = f"{benchmark_dir}filter_aggregate_directories_{token}.txt"
         rule += _get_benchmark(benchmark_out)
     rule += _get_main(dirs_in, dir_out)
     return rule
