2c23bba73f1132041519dec361545772af7c5ccd
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-06-05 17:52:45 +0200

    added phi for two groups

--- nodes/benchmark/similarity/__init__.py
+++ nodes/benchmark/similarity/__init__.py
@@ -3,7 +3,7 @@ import secrets
 
 def _get_header(token):
     return f'''
-rule benchmark_diversity_{token}:
+rule benchmark_similarity_{token}:
 '''
 
 
@@ -14,29 +14,29 @@ def _get_benchmark(benchmark_out):
 '''
 
 
-def _get_main(group_1_in, group_2_in, csv_out):
+def _get_main(group_1_in, group_2_in, corr_dir_out):
     return f'''\
     input:
         group_1_in="{group_1_in}",
         group_2_in="{group_2_in}"
     output:
-        csv_out="{csv_out}"
+        corr_dir_out="{corr_dir_out}"
     threads:
          1000      
     params:
-        snakefile="nodes/benchmark/diversity/Snakefile",
-        configfile="nodes/benchmark/diversity/config.yaml"
+        snakefile="nodes/benchmark/similarity/Snakefile",
+        configfile="nodes/benchmark/similarity/config.yaml"
     run:
         with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
             shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
 '''
 
 
-def rule(group_1_in, group_2_in, csv_out, benchmark_dir=None):
+def rule(group_1_in, group_2_in, corr_dir_out, benchmark_dir=None):
     token = secrets.token_hex(4)
     rule = _get_header(token)
     if benchmark_dir is not None:
-        benchmark_out = f"{benchmark_dir}benchmark_cross_validation_ensemble_{token}.txt"
+        benchmark_out = f"{benchmark_dir}benchmark_similarity_{token}.txt"
         rule += _get_benchmark(benchmark_out)
-    rule += _get_main(group_1_in, group_2_in, csv_out)
+    rule += _get_main(group_1_in, group_2_in, corr_dir_out)
     return rule
