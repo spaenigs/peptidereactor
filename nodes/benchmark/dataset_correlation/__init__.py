b5847dc12b23893ad9e1e02460af065c179bf4a7
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-07-17 16:26:55 +0200

    initial commit: compute dataset correlation

---
+++ nodes/benchmark/dataset_correlation/__init__.py
@@ -0,0 +1,42 @@
+import secrets
+
+
+def _get_header(token):
+    return f'''
+rule benchmark_dataset_correlation_{token}:
+'''
+
+
+def _get_benchmark(benchmark_out):
+    return f'''\
+    benchmark:
+        "{benchmark_out}"
+'''
+
+
+def _get_main(group_1_in, group_2_in, dataset_corr_out):
+    return f'''\
+    input:
+        group_1_in="{group_1_in}",
+        group_2_in="{group_2_in}"
+    output:
+        dataset_corr_out="{dataset_corr_out}"
+    threads:
+         1000      
+    params:
+        snakefile="nodes/benchmark/dataset_correlation/Snakefile",
+        configfile="nodes/benchmark/dataset_correlation/config.yaml"
+    run:
+        with WorkflowExecuter(dict(input), dict(output), params.configfile, cores=CORES) as e:
+            shell(f"""{{e.snakemake}} -s {{params.snakefile}} --configfile {{params.configfile}}""")
+'''
+
+
+def rule(group_1_in, group_2_in, dataset_corr_out, benchmark_dir=None):
+    token = secrets.token_hex(4)
+    rule = _get_header(token)
+    if benchmark_dir is not None:
+        benchmark_out = f"{benchmark_dir}benchmark_dataset_correlation_{token}.txt"
+        rule += _get_benchmark(benchmark_out)
+    rule += _get_main(group_1_in, group_2_in, dataset_corr_out)
+    return rule
---
+++ nodes/benchmark/dataset_correlation/scripts/dataset_correlation.R
@@ -0,0 +1,58 @@
+library(gtools)
+library(foreach)
+library(doParallel)
+
+files <- c(
+  Sys.glob(paste0(snakemake@input[["group_1"]], "*.csv")),
+  Sys.glob(paste0(snakemake@input[["group_2"]], "*.csv"))
+)
+
+name_pairs <- combinations(length(files),2, files, repeats=FALSE)
+
+cl <- makeCluster(snakemake@params$cores)
+registerDoParallel(cl)
+
+chunks <- split(1:dim(name_pairs)[1], sort(1:dim(name_pairs)[1]%%10000))
+
+df_res <- data.frame()
+
+for (chunk in chunks[1:10]) {
+
+  finalMatrix <- foreach(i=chunk, .combine=rbind) %dopar% {
+
+    library(MatrixCorrelation)
+
+    n1 <- as.character(name_pairs[i, 1])
+    n2 <- as.character(name_pairs[i, 2])
+
+    X1 <- read.csv(n1, row.names = 1)
+    X2 <- read.csv(n2, row.names = 1)
+
+    if (dim(X1)[1] > dim(X2)[1]) {
+      m1 <- data.matrix(X1[rownames(X2), -which(names(X1) == "y")])
+      m2 <- data.matrix(X2[            , -which(names(X2) == "y")])
+    } else if (dim(X1)[1] < dim(X2)[1]) {
+      m1 <- data.matrix(X1[            , -which(names(X1) == "y")])
+      m2 <- data.matrix(X2[rownames(X1), -which(names(X2) == "y")])
+    } else {
+      m1 <- data.matrix(X1[ , -which(names(X1) == "y")])
+      m2 <- data.matrix(X2[ , -which(names(X2) == "y")])
+    }
+
+    df <- data.frame(
+      x = gsub(".*/(.*)\\.csv", "\\1", n1),
+      y = gsub(".*/(.*)\\.csv", "\\1", n2),
+      res = 1 - RVadjMaye(m1, m2)
+    )
+
+    na.omit(df)
+  }
+
+  token <- paste0(sample(c(0:9, LETTERS[1:6]), 8, T), collapse = '')
+  df_res <- rbind(df_res, finalMatrix)
+  write.csv(df_res, paste0(snakemake@output[[2]], "/dataset_correlation_part_", token, ".csv"))
+}
+
+stopCluster(cl)
+
+write.csv(df_res, snakemake@output[[1]])
