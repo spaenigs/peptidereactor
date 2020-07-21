294e54717664bd2e2829583961d565cda1d8f4f8
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-07-21 18:11:23 +0200

    added further visualizations

--- nodes/vis/single_dataset/__init__.py
+++ nodes/vis/single_dataset/__init__.py
@@ -12,24 +12,21 @@ def _get_benchmark(benchmark_out):
         "{benchmark_out}"'''
 
 
-def _get_main(fasta_in, classes_in,
-              metrics_dir_in, dataset_correlation_in,
-              similarity_dir_group_1_in, similarity_dir_group_2_in,
-              ensemble_cv_group_1a_in, ensemble_cv_group_2a_in,
-              ensemble_cv_group_1b_in, ensemble_cv_group_2b_in,
-              html_out):
+def _get_main(fasta_in, classes_in, encoding_benchmark_dir_in, html_out):
     return f'''
     input:
          fasta_in="{fasta_in}",
          classes_in="{classes_in}",
-         metrics_dir_in="{metrics_dir_in}",
-         dataset_correlation_in="{dataset_correlation_in}",
-         similarity_dir_group_1_in="{similarity_dir_group_1_in}",
-         similarity_dir_group_2_in="{similarity_dir_group_2_in}",
-         ensemble_cv_group_1a_in="{ensemble_cv_group_1a_in}", 
-         ensemble_cv_group_2a_in="{ensemble_cv_group_2a_in}",
-         ensemble_cv_group_1b_in="{ensemble_cv_group_1b_in}", 
-         ensemble_cv_group_2b_in="{ensemble_cv_group_2b_in}"                
+         metrics_dir_in="{encoding_benchmark_dir_in + "metrics/"}",
+         dataset_correlation_in="{encoding_benchmark_dir_in + "dataset_correlation.csv"}",
+         similarity_dir_group_1_in="{encoding_benchmark_dir_in + "similarity/seq_vs_str/"}",
+         similarity_dir_group_2_in="{encoding_benchmark_dir_in + "similarity/all_vs_all/"}",
+         ensemble_cv_group_1a_in="{encoding_benchmark_dir_in + "ensemble/seq_vs_str/sequence_based/"}", 
+         ensemble_cv_group_2a_in="{encoding_benchmark_dir_in + "ensemble/seq_vs_str/structure_based/"}",
+         ensemble_cv_group_1b_in="{encoding_benchmark_dir_in + "ensemble/all_vs_all/group_1/"}", 
+         ensemble_cv_group_2b_in="{encoding_benchmark_dir_in + "ensemble/all_vs_all/group_2/"}",
+         crit_diff_in="{encoding_benchmark_dir_in + "friedman/diff_matrix.csv"}", 
+         nemenyi_in="{encoding_benchmark_dir_in + "friedman/nemenyi.yaml"}"
     output:
          html_out="{html_out}"
     threads:
@@ -43,21 +40,12 @@ def _get_main(fasta_in, classes_in,
 '''
 
 
-def rule(fasta_in, classes_in,
-         metrics_dir_in, dataset_correlation_in,
-         similarity_dir_group_1_in, similarity_dir_group_2_in,
-         ensemble_cv_group_1a_in, ensemble_cv_group_2a_in,
-         ensemble_cv_group_1b_in, ensemble_cv_group_2b_in,
-         html_out,
+def rule(fasta_in, classes_in, encoding_benchmark_dir_in, html_out,
          benchmark_dir=None):
     token = secrets.token_hex(4)
     rule = _get_header(token)
     if benchmark_dir is not None:
         benchmark_out = f"{benchmark_dir}vis_single_dataset_{token}.txt"
         rule += _get_benchmark(benchmark_out)
-    rule += _get_main(fasta_in, classes_in, metrics_dir_in, dataset_correlation_in,
-                      similarity_dir_group_1_in, similarity_dir_group_2_in,
-                      ensemble_cv_group_1a_in, ensemble_cv_group_2a_in,
-                      ensemble_cv_group_1b_in, ensemble_cv_group_2b_in,
-                      html_out)
+    rule += _get_main(fasta_in, classes_in, encoding_benchmark_dir_in, html_out)
     return rule
