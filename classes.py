3891914d0b1e6f72d21b654b5383005c96e4339f
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-07-30 17:49:06 +0200

    tried ratio

--- classes.py
+++ classes.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import altair as alt
 
+from vega_datasets import data
 from modlamp.core import read_fasta
 
 # dataset = "hiv_protease"
-# dataset = "ace_vaxinpad"
-dataset = "npp_profet"
+dataset = "ace_vaxinpad"
+# dataset = "npp_profet"
+# dataset = "afp_amppred"
 
 seqs, names = read_fasta(f"data/{dataset}/seqs.fasta")
 with open(f"data/{dataset}/classes.txt") as f:
@@ -33,29 +35,55 @@ source["range"] = source.index
 dfm = pd.melt(source, id_vars=["range"], value_vars=list(source.columns)[:-1],
               var_name="Class", value_name="Length")
 
+dfm.groupby("Class").apply(lambda df: (df["Length"]/df["Length"].sum())*100).to_frame("ratio")
+
 base = alt.Chart(dfm)
 
-height = 15 * len(set(source.index))
+range = [0, dfm["Length"].sort_values().unique()[-1]]
 
-middle = base.mark_text().encode(
-    y=alt.Y('range:O', axis=None),
-    text=alt.Text('range:Q'),
-).properties(height=height)
+height = 450
 
 left = base.mark_bar().transform_filter(
     "datum.Class == 'Positive'"
 ).encode(
-    y=alt.Y('range:O', axis=None),
-    x=alt.X('Length:Q', sort=alt.Sort("descending"))
+    x=alt.X('Length:Q', scale=alt.Scale(domain=range), sort=alt.Sort("descending")),
+    y=alt.Y('range:O'),
+    color="Class:N"
 ).properties(height=height)
 
 right = base.mark_bar().transform_filter(
     "datum.Class == 'Negative'"
 ).encode(
     y=alt.Y('range:O', axis=None),
-    x=alt.X('Length:Q')
+    x=alt.X('Length:Q', scale=alt.Scale(domain=range)),
+    color="Class:N"
 ).properties(height=height)
 
-# TODO if too big change view
+cnts = dfm.groupby(by="Class")["Length"].sum().to_frame("nr")
+cnts["Class"] = cnts.index
 
-alt.concat(left, middle, right, spacing=5).save("chart.html")
+range2 = [0, cnts["nr"].max()]
+print(range2)
+
+bar_left = alt.Chart(cnts).mark_bar().transform_filter(
+    "datum.Class == 'Positive'"
+).encode(
+    y=alt.Y('Class:N'),
+    x=alt.X('nr:Q', scale=alt.Scale(domain=range2), sort=alt.Sort("descending")),
+    color="Class:N"
+).properties(height=20)
+
+bar_right = alt.Chart(cnts).mark_bar().transform_filter(
+    "datum.Class == 'Negative'"
+).encode(
+    y=alt.Y('Class:N', axis=None),
+    x=alt.X('nr:Q', scale=alt.Scale(domain=range2)),
+    color="Class:N"
+).properties(height=20)
+
+c1 = alt.hconcat(left, right, title=alt.TitleParams(text="All (left) vs. within group (right)", anchor="middle"))
+c2 = alt.hconcat(bar_left, bar_right, title=alt.TitleParams(text="All (left) vs. within group (right)", anchor="middle"))
+
+alt.vconcat(c1, c2, spacing=0).save("chart.html")
+
+
