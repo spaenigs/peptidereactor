d11d23b01f2929c57f41fb5a3ea920ce1ad1cf41
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-08-03 10:26:08 +0200

    fixed issue with gaps in residue indices

--- nodes/encodings/qsar/scripts/compute_qsar.py
+++ nodes/encodings/qsar/scripts/compute_qsar.py
@@ -7,10 +7,21 @@ import pandas as pd
 import numpy as np
 import joblib as jl
 
+import warnings
+
+
+def reset_indices(chain):
+    for i, residue in enumerate(chain.get_residues(), start=1):
+        res_id = list(residue.id)
+        res_id[1] = i
+        residue.id = tuple(res_id)
+    return chain
+
 
 def get_pdb_chunks(full_pdb, window_size, len_residues, token):
     len_residues += 1
     for chain in full_pdb.get_chains():
+        chain = reset_indices(chain)
         chain_id = chain.get_id()
         start_idx, stop_idx = \
             list(chain.get_residues())[0].get_id()[1], \
@@ -18,18 +29,13 @@ def get_pdb_chunks(full_pdb, window_size, len_residues, token):
         if len_residues <= window_size:
             windows = [[start_idx, stop_idx]]
         else:
-            tmp = enumerate(range(window_size, len_residues))
-            windows = [(i+start_idx, j+start_idx) for i, j in tmp]
+            windows = enumerate(range(window_size, len_residues))
         for start, end in windows:
             filename = f"data/temp/{token}/{full_pdb.get_id()}_{start}_{end}.pdb"
             Dice.extract(full_pdb, chain_id, start, end, filename)
             mol_obj = Chem.MolFromPDBFile(filename)
-            yield filename, mol_obj
-
-
-def clean_up(filenames):
-    for path in filenames:
-        shell(f"rm {path}")
+            shell(f"rm {filename}")
+            yield mol_obj
 
 
 smk_obj = snakemake
@@ -40,20 +46,18 @@ name, path, token = \
 structure = PDBParser().get_structure(name, path)
 seqs, class_ = jl.load(smk_obj.input[1])
 
-calc = Calculator(descriptors)
 len_residues = len(list(structure.get_residues()))
-filenames, pdbs = zip(*get_pdb_chunks(structure, 20, len_residues, token))
+pdbs = get_pdb_chunks(structure, 20, len_residues, token)
 
 try:
+    warnings.filterwarnings("ignore")
+    calc = Calculator(descriptors)
     df = calc.pandas(list(pdbs), quiet=True)
     df = df.loc[:, [s.dtype in [np.float, np.int] for n, s in df.items()]]
     df.dropna(axis="columns", inplace=True)
     df = pd.DataFrame(df.apply(np.mean)).transpose()
     df.index = [name]
     df["y"] = [class_]
-
     df.to_csv(str(smk_obj.output))
-
-    clean_up(filenames)
 except Exception as e:
     print(e)
