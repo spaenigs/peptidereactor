bb654687b75af5973781392fecf5ba4be43f4041
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-09-14 09:31:17 +0200

    check for data presence

--- datasets_size.py
+++ datasets_size.py
@@ -3,9 +3,12 @@ from modlamp.core import read_fasta
 
 import pandas as pd
 
-df = pd.DataFrame([(d, len(read_fasta(d)[1])) for d in glob("data/*/seqs.fasta")])
+import os
+
+df = pd.DataFrame([(d.replace("seqs.fasta", ""), len(read_fasta(d)[1])) for d in glob("data/*/seqs.fasta")])
 df.columns = ["path", "nr_of_seqs"]
-df.sort_values(by="nr_of_seqs", inplace=True)
+df["present"] = df["path"].apply(lambda p: os.path.exists(p + "pdb/"))
+df.sort_values(by=["nr_of_seqs", "present"], inplace=True)
 df.reset_index(inplace=True)
 
 print(df)
