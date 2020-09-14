02596c51acd7b3c2a9c62ac00f47487a8de23f0f
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-09-14 12:59:27 +0200

    check for benchmark presence

--- datasets_size.py
+++ datasets_size.py
@@ -7,9 +7,9 @@ import os
 
 df = pd.DataFrame([(d.replace("seqs.fasta", ""), len(read_fasta(d)[1])) for d in glob("data/*/seqs.fasta")])
 df.columns = ["path", "nr_of_seqs"]
-df["present"] = df["path"].apply(lambda p: os.path.exists(p + "pdb/"))
-df.sort_values(by=["nr_of_seqs", "present"], inplace=True)
+df["enc_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "pdb/")))
+df["ben_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "benchmark/dataset_correlation.csv")))
+df.sort_values(by="nr_of_seqs", inplace=True)
 df.reset_index(inplace=True)
 
 print(df)
-
