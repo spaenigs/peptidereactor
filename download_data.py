acf13998e74afad878da55b7d95d246a3cc3456e
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-09-18 16:05:34 +0200

    downloading benchmark

--- download_data.py
+++ download_data.py
@@ -3,6 +3,15 @@ from glob import glob
 import subprocess
 import os
 
+
+def exec_cmd(cmd):
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, cwd="/home/spaenigs/")
+    return process.communicate()
+
+# dir = "/home/spaenigs/PycharmProjects/"
+dir = "/media/spaenigs/4B1DB7375F3291A1/"
+
+
 i = 203
 
 for p in glob("data/*"):
@@ -12,12 +21,16 @@ for p in glob("data/*"):
         bash_command = f"ssh ubuntu@172.16.103.{i} " \
                        f"test -e /home/ubuntu/peptidereactor/{p}/benchmark/dataset_correlation.csv && " \
                        f"echo $?"
-        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE, cwd="/home/spaenigs/")
-        output, _ = process.communicate()
+        output, _ = exec_cmd(bash_command)
         # only download files, if present remote and not already downloaded
         if output.decode('ascii') and not os.path.exists(f"{p}/benchmark/dataset_correlation.csv"):
             print(f"Downloading {p}...")
+            # benchmark data
+            exec_cmd(f"mkdir -p {p}/misc/benchmark/")
+            bash_command = f"scp ubuntu@172.16.103.{i}:/home/ubuntu/peptidereactor/{p}/misc/benchmark/*  " \
+                           f"{dir}peptidereactor/{p}/misc/benchmark/"
+            exec_cmd(bash_command)
             bash_command = f"scp -r ubuntu@172.16.103.{i}:/home/ubuntu/peptidereactor/{p}/benchmark/  " \
-                           f"/home/spaenigs/PycharmProjects/peptidereactor/{p}"
-            process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE, cwd="/home/spaenigs/")
-            output, error = process.communicate()
+                           f"{dir}/peptidereactor/{p}"
+            exec_cmd(bash_command)
+
