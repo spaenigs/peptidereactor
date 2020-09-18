09377213ea06666b4683b921aaa6d9eb7f4af6cd
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-09-18 12:26:18 +0200

    updated enc check

--- datasets_size.py
+++ datasets_size.py
@@ -7,7 +7,7 @@ import os
 
 df = pd.DataFrame([(d.replace("seqs.fasta", ""), len(read_fasta(d)[1])) for d in glob("data/*/seqs.fasta")])
 df.columns = ["path", "nr_of_seqs"]
-df["enc_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "pdb/")))
+df["enc_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "csv/structure_based/")))
 df["ben_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "benchmark/dataset_correlation.csv")))
 df.sort_values(by="nr_of_seqs", inplace=True)
 df.reset_index(drop=True, inplace=True)
