45532ee09aa6235986fbfc10107a8e36a2a5f64e
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-09-24 12:49:09 +0200

    general improvements

--- bcm_september_final_plot.py
+++ bcm_september_final_plot.py
@@ -16,14 +16,20 @@ input_dropdown = alt.binding_select(options=list(seq_names) + [None])
 selection = alt.selection_single(fields=['seq_name'], bind=input_dropdown, name="_")
 
 alt.Chart(source).mark_point(filled=True, shape="square", size=300, opacity=1.0).encode(
-    x=alt.X("x:N", sort=alt.Sort(alt.SortArray(xvals))),
-    y=alt.Y("y:N", sort=alt.Sort(alt.SortArray(source.sort_values(["rank", "y"])["y"].unique()))),
+    x=alt.X("x:N", title=None, sort=alt.Sort(alt.SortArray(xvals))),
+    y=alt.Y("y:N", title="Lab results vs. prediction", sort=alt.Sort(alt.SortArray(source.sort_values(["rank", "y"])["y"].unique()))),
     color=alt.Color("coupl:N", title="% Ac"),
     tooltip="seq:N"
 ).add_selection(
     selection
 ).transform_filter(
     selection
+).configure_axis(
+    labelLimit=350,
+    labelFontSize=12,
+    titlePadding=150
 ).properties(
+    title=["Comparison of difficult couplings observed in the lab vs. predictions",
+           "Window length: 7, cut-off: 1.5 %"],
     width=700
 ).save("chart.html")
