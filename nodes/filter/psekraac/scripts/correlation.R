bfd7947f0022bf0da0da4dbdd2755bfee08ba5be
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-10-07 09:26:43 +0200

    replaced with RVadj

---
+++ nodes/filter/psekraac/scripts/correlation.R
@@ -0,0 +1,45 @@
+library(foreach)
+library(doParallel)
+library(gtools)
+
+cores <- detectCores()
+cl <- makeCluster(cores)
+
+registerDoParallel(cl)
+
+files <- unlist(snakemake@input)
+c <- combinations(length(files), 2, files)
+
+finalMatrix <- foreach(i=1:dim(c)[1], .combine=rbind) %dopar% {
+
+  csv_path_1 <- c[i,1]
+  csv_path_2 <- c[i,2]
+
+  X1 <- read.csv(csv_path_1, row.names = 1)
+  X2 <- read.csv(csv_path_2, row.names = 1)
+
+  if (dim(X1)[1] > dim(X2)[1]) {
+    m1 <- data.matrix(X1[rownames(X2), -which(names(X1) == "y")])
+    m2 <- data.matrix(X2[            , -which(names(X2) == "y")])
+  } else if (dim(X1)[1] < dim(X2)[1]) {
+    m1 <- data.matrix(X1[            , -which(names(X1) == "y")])
+    m2 <- data.matrix(X2[rownames(X1), -which(names(X2) == "y")])
+  } else {
+    m1 <- data.matrix(X1[ , -which(names(X1) == "y")])
+    m2 <- data.matrix(X2[ , -which(names(X2) == "y")])
+  }
+
+  library(MatrixCorrelation)
+
+  data.frame(
+    e1 = csv_path_1,
+    e2 = csv_path_2,
+    R = RVadjMaye(m1, m2)
+  )
+
+}
+
+stopCluster(cl)
+
+write.csv(finalMatrix, snakemake@output[[1]])
+
