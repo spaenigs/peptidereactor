692c34f9dde5cb8ae27ddb76c6d11ef6996a694d
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-10-23 17:20:57 +0200

    added filter

--- datasets_size.py
+++ datasets_size.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 import os
 
-df = pd.DataFrame([(d.replace("seqs.fasta", ""), len(read_fasta(d)[1])) for d in glob("data/*/seqs.fasta")])
+df = pd.DataFrame([(d.replace("seqs.fasta", ""), len(read_fasta(d)[1])) for d in glob("data/*/seqs.fasta") if ("bcm" not in d)])
 df.columns = ["path", "nr_of_seqs"]
 df["enc_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "csv/structure_based/")))
 df["ben_pres"] = df["path"].apply(lambda p: int(os.path.exists(p + "benchmark/dataset_correlation.csv")))
 df.sort_values(by="nr_of_seqs", inplace=True)
 df.reset_index(drop=True, inplace=True)
 
-print(df)
+print(df.loc[(df.enc_pres == 0) | (df.ben_pres == 0), :])
