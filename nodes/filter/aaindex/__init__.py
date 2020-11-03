b87865fa903ac87e16385cb66167f92f7f27b5ae
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-11-03 12:59:00 +0100

    added doc string

--- nodes/filter/aaindex/__init__.py
+++ nodes/filter/aaindex/__init__.py
@@ -30,6 +30,18 @@ def _get_main(csv_in, csv_out):
 
 
 def rule(csv_in, csv_out, benchmark_dir=None):
+    """
+    Filters encoded amino acid index (aaindex)-based encodings, by omitting highly correlated
+    indices.
+
+    Category: filter. \n
+    Node: aaindex
+
+    :param csv_in: The path to the directory containing the original aaindex-based datasets.
+    :param csv_out: The path to the output directory, which contains the filtered datasets.
+    :param benchmark_dir: The path to the directory to store the benchmark results. If None,
+           benchmark will be not executed (default).
+    """
     token = secrets.token_hex(4)
     rule = _get_header(token)
     if benchmark_dir is not None:
