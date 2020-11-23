b28221d237e274bd23ca19db0ecf687bf50dff6f
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2020-11-23 12:27:34 +0100

    deleted node: tertiary structure prediction

--- nodes/utils/tertiary_structure_prediction/scripts/unzip_databases.py
+++
@@ -1,9 +0,0 @@
-from snakemake import shell
-
-db = snakemake.wildcards.database
-if db in ["nr70", "nr90"]:
-    target_dir = f"peptidereactor/RaptorX/databases/" + db
-else:
-    target_dir = f"peptidereactor/RaptorX/databases/"
-shell(f"""tar -zxf {{input}} -C {target_dir};
-           touch {snakemake.output[0]}""")
