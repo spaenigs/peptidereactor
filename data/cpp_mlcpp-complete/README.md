d8325841e9ed5c1ca05948503d01b8e2b773f8f3
Sebastian Spänig <sebastian.spaenig@uni-marburg.de>
2021-02-08 16:19:38 +0100

    added dataset; fixed import issue

---
+++ data/cpp_mlcpp-complete/README.md
@@ -0,0 +1,20 @@
+# Cell-penetrating peptides vs. CPP (complete)
+
+##### Description 
+
+    The dataset is taken from [1] and downloaded from http://www.thegleelab.org/MLCPP/MLCPPData.html. Uses and 
+    distinguishes between train and test data as stated in [1].
+    
+##### Reference
+
+    [1] Manavalan B., Subramaniyam S., Shin TH, Kim MO, Lee G., Machine-Learning-Based Prediction of Cell-Penetrating 
+        Peptides and Their Uptake Efficiency with Improved Accuracy. Journal of Proteome Research (2018), 17 (8):
+        10.1021/acs.jproteome.8b00148 
+        
+##### Summary
+ 
+| Description                                                               | Reference                         |
+|---------------------------------------------------------------------------|-----------------------------------|
+| Machine-Learning-Based Prediction of Cell-Penetrating Peptides and Their Uptake Efficiency with Improved Accuracy. | https://doi.org/10.1021/acs.jproteome.8b00148 |
+             
+
--- main.py
+++ main.py
@@ -19,63 +19,67 @@ TOKEN = secrets.token_hex(6)
 
 CORES = 32
 DATASETS = [
-    # mosla
-    "cpp_mixed",
-    "amp_gonzales",
-    "cpp_sanders",
-    "hiv_bevirimat",
-    "tce_zhao",
-    "amp_fernandes",
-    "hiv_nfv",
-    "hiv_v3",
-
-    # 179
-    "amp_csamp",
-    "acp_iacp",
-    "cpp_mlcppue",
-    "acp_anticp",
-    "atb_antitbp",
-    "hiv_lpv",
-    "acp_mlacp",
-    "hiv_abc",
-    "hiv_azt",
-    "hiv_d4t",
-    "hiv_ddi",
-    "hiv_3tc",
-    "amp_iamp2l",
-    "hem_hemopi",
-    "aip_aippred",
-
-    # 199
-    "hiv_apv",
-    "hiv_dlv",
-    "hiv_efv",
-    "hiv_rtv",
-    "hiv_nvp",
-    "hiv_idv",
-    "amp_antibp",
-    "cpp_cppredfl",
-    "hiv_protease",
-    "cpp_kelmcpp",
-    "avp_avppred",
-    "cpp_cellppdmod",
-    "pip_pipel",
-    "hiv_sqv",
-    "atb_iantitb",
 
     # 203
-    "avp_amppred",
-    "cpp_cellppd",
-    "nep_neuropipred",
-    "cpp_mlcpp",
-    "amp_antibp2",
-    "bce_ibce",
-    "amp_modlamp",
-    "afp_amppred",
-    "afp_antifp",
-    "isp_il10pred",
-    "ace_vaxinpad",
-    "aip_antiinflam"
+    "cpp_mlcpp-complete"
+
+    # # mosla
+    # "cpp_mixed",
+    # "amp_gonzales",
+    # "cpp_sanders",
+    # "hiv_bevirimat",
+    # "tce_zhao",
+    # "amp_fernandes",
+    # "hiv_nfv",
+    # "hiv_v3",
+    #
+    # # 179
+    # "amp_csamp",
+    # "acp_iacp",
+    # "cpp_mlcppue",
+    # "acp_anticp",
+    # "atb_antitbp",
+    # "hiv_lpv",
+    # "acp_mlacp",
+    # "hiv_abc",
+    # "hiv_azt",
+    # "hiv_d4t",
+    # "hiv_ddi",
+    # "hiv_3tc",
+    # "amp_iamp2l",
+    # "hem_hemopi",
+    # "aip_aippred",
+    #
+    # # 199
+    # "hiv_apv",
+    # "hiv_dlv",
+    # "hiv_efv",
+    # "hiv_rtv",
+    # "hiv_nvp",
+    # "hiv_idv",
+    # "amp_antibp",
+    # "cpp_cppredfl",
+    # "hiv_protease",
+    # "cpp_kelmcpp",
+    # "avp_avppred",
+    # "cpp_cellppdmod",
+    # "pip_pipel",
+    # "hiv_sqv",
+    # "atb_iantitb",
+    #
+    # # 203
+    # "avp_amppred",
+    # "cpp_cellppd",
+    # "nep_neuropipred",
+    # "cpp_mlcpp",
+    # "amp_antibp2",
+    # "bce_ibce",
+    # "amp_modlamp",
+    # "afp_amppred",
+    # "afp_antifp",
+    # "isp_il10pred",
+    # "ace_vaxinpad",
+    # "aip_antiinflam"
 ]
 
 with WorkflowSetter(cores=CORES, benchmark_dir="data/{dataset}/misc/benchmark/") as w:
